cf0968726b8eb5611ad0696e1a724b97dc233591
Leecoin49 <slkofficial@yeah.net>
2025-12-13 16:26:34 +0800

    Add Go Learning

---
+++ goLearning/TheGoProgrammingLanguage/1.2.Command-LineArguments/echoPractice1.go
@@ -0,0 +1,22 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// func main() {
+// 	// can print os.Args[0]
+// 	var s, sep string
+
+// 	for i := 0; i < len(os.Args); i++ {
+// 		s += sep + os.Args[i]
+// 		sep = " "
+// 	}
+
+// 	fmt.Println(strings.Repeat("*", 50))
+// 	fmt.Println(os.Args[0])
+// 	fmt.Println(strings.Repeat("*", 50))
+// 	fmt.Println(s)
+// }
